892b28b1fa5cd923a569cb935ee99be071037000
sanghmitra2023 <128592793+sanghmitra2023@users.noreply.github.com>
2023-06-03 12:23:25 +0000

    complete the js code

--- src/app.js
+++ src/app.js
@@ -5,7 +5,35 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+const totalCards = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+const suits = ["♦", "♥", "♠", "♣"];
+
+function getRandomElementFromArray(array) {
+  return array[Math.floor(Math.random() * array.length)];
+}
+const randomSuits = getRandomElementFromArray(suits);
+const randomNumber = getRandomElementFromArray(totalCards);
+
+const randomNumberElement = document.getElementById("randomNumber");
+randomNumberElement.innerHTML = randomNumber;
+
+const randomSuitElement = document.querySelector(".randomSuit");
+randomSuitElement.innerHTML = randomSuits;
+
+const randomSuitElementTwo = document.querySelector(".randomSuitTwo");
+randomSuitElementTwo.innerHTML = randomSuits;
+
+if (randomSuits === "♦" || randomSuits === "♥") {
+  randomSuitElement.style.color = "red";
+  randomNumberElement.style.color = "red";
+  randomSuitElementTwo.style.color = "red";
+}
+
+document.getElementById("button").addEventListener("click", function() {
+  window.location.reload();
+});
+
+function reloadThePage() {
+  window.location.reload();
+}
+setInterval(reloadThePage, 10000);
